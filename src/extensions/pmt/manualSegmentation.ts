6ee1b3bb778080f5e6e8dd5c6d95840061c11be7
STEN <sten.li@foxmail.com>
2025-01-08 10:50:32 +0800

    Update manualSegmentation.ts

--- src/extensions/pmt/manualSegmentation.ts
+++ src/extensions/pmt/manualSegmentation.ts
@@ -8,40 +8,127 @@ useExtensionService().registerExtension({
       return
     }
 
+    const getVolViewUrl = () => {
+      // eslint-disable-next-line prefer-const
+      let { origin, port, pathname } = document.location
+      if (origin === 'file://') {
+        pathname = pathname.replace('comfyui/', 'volview/')
+      } else {
+        if (port) {
+          origin = origin.replace(port, `${+port - 1 - 2}`)
+        }
+        pathname += '#/volview/'
+      }
+      const query = new URLSearchParams(document.location.search)
+      let search = `?defaultTool=Paint&drawer=permanent`
+      const pipelineId = query.get('pipelineId')
+      search += pipelineId ? `&pipelineId=${pipelineId}` : ''
+      // search += `&manualNodeId=${node.id}`
+      return new URL(origin + pathname + search).href
+    }
+
+    const getInputs = () => {
+      const inputs = []
+      const inputNode = node.getInputNode(0)
+      if (inputNode) {
+        // @ts-expect-error custom pmt_fields
+        const pmt_fields = inputNode.pmt_fields as any
+        if (pmt_fields?.status === 'done') {
+          if (pmt_fields.outputs?.length) {
+            inputs.push(...pmt_fields.outputs)
+          }
+        }
+      }
+      return inputs
+    }
+    node['getInputs_'] = getInputs
+
+    const getOutputs = () => {
+      const outputs = []
+      const outputInfo = node.getOutputInfo(0)
+      if (outputInfo?.type === 'NIFTI_FILE') {
+        // @ts-expect-error custom pmt_fields
+        const pmt_fields = node.pmt_fields as any
+        if (pmt_fields) {
+          if (pmt_fields.outputs?.length) {
+            outputs.push(...pmt_fields.outputs)
+          }
+        }
+      }
+      return outputs
+    }
+    node['getOutputs_'] = getOutputs
+
+    const div = document.createElement('div')
+    div.classList.add(
+      'relative',
+      'overflow-hidden',
+      'flex',
+      'items-center',
+      'justify-between'
+    )
+    const toggleEl = document.createElement('button')
+    toggleEl.textContent = 'Open VolView'
+    toggleEl.style.visibility = 'hidden'
+    toggleEl.classList.add('text-xs', 'hover:cursor-pointer')
+    div.appendChild(toggleEl)
+    const countEl = document.createElement('span')
+    countEl.textContent = `0/0`
+    countEl.style.visibility = 'hidden'
+    countEl.classList.add('text-sm')
+    div.appendChild(countEl)
+    const continueEl = document.createElement('button')
+    continueEl.textContent = 'Continue'
+    continueEl.style.visibility = 'hidden'
+    continueEl.classList.add('text-xs', 'hover:cursor-pointer')
+    div.appendChild(continueEl)
+
+    const widget = node.addDOMWidget(
+      'manual-segmentation',
+      'manual-segmentation',
+      div,
+      {}
+    )
+
     const _onDrawBackground = node.onDrawBackground
     node.onDrawBackground = function (...args) {
       // @ts-expect-error custom pmt_fields
       const pmt_fields = node.pmt_fields as any
       const isWaiting = pmt_fields?.status === 'waiting'
 
-      const dicomInputNode = node.getInputNode(0)
+      if (isWaiting) {
+        const inputs = node['getInputs_']()
+        const outputs = node['getOutputs_']()
 
-      let dicomPath, dicomOid
-      if (dicomInputNode && isWaiting) {
-        // @ts-expect-error custom pmt_fields
-        const pmt_fields = dicomInputNode.pmt_fields as any
-        if (pmt_fields?.status === 'done') {
-          const dicomInputs = pmt_fields.outputs || []
-          if (Array.isArray(dicomInputs[0]?.path)) {
-            dicomPath = dicomInputs[0]?.path?.[0]
-          } else {
-            dicomPath = dicomInputs[0]?.path
+        const inputCount = inputs.filter((input) => !!input.path).length
+        const outputCount = outputs.filter((output) => !!output.path).length
+
+        if (inputCount > 0) {
+          countEl.textContent = `${outputCount}/${inputCount}`
+          if (!toggleEl['openVolView']) {
+            toggleEl['openVolView'] = (e) => {
+              const VOLVIEW_URL = getVolViewUrl() + `&manualNodeId=${node.id}`
+              window.open(VOLVIEW_URL, '_blank')
+            }
+            toggleEl.onclick = toggleEl['openVolView']
           }
-          if (Array.isArray(dicomInputs[0]?.oid)) {
-            dicomOid = dicomInputs[0]?.oid?.[0]
-          } else {
-            dicomOid = dicomInputs[0]?.oid
+          toggleEl.style.visibility = 'visible'
+          countEl.style.visibility = 'visible'
+          if (outputCount === inputCount) {
+            continueEl.style.visibility = 'visible'
+            continueEl.onclick = (e) => {
+              console.log('continue run...')
+            }
           }
+        } else {
+          toggleEl.style.visibility = 'hidden'
+          countEl.style.visibility = 'hidden'
+          continueEl.style.visibility = 'hidden'
         }
-      }
-
-      if (dicomPath || dicomOid) {
-        console.log('waiting...', '0/1', {
-          oid: dicomOid,
-          path: dicomPath
-        })
-
-        // ...
+      } else {
+        toggleEl.style.visibility = 'hidden'
+        countEl.style.visibility = 'hidden'
+        continueEl.style.visibility = 'hidden'
       }
 
       return _onDrawBackground?.apply(this, args)

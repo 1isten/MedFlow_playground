adec2820573b51381b1c5fbfe141d3b3fa1b9c6c
STEN <sten.li@foxmail.com>
2025-06-03 15:56:19 +0800

    Update pmtCore.ts

--- src/constants/pmtCore.ts
+++ src/constants/pmtCore.ts
@@ -4,12 +4,12 @@ export const DATATYPE_COLOR = {
   FLOAT: '#7F7', // green
   STRING: '#77F', // purple
 
-  '1D': '#facc15', // yellow (Array, 1D np.ndarray)
+  '1D': '#facc15', // yellow (Array, 1D np.NdArray)
   TABLE: '#a3e635', // lime (DataFrame, pandas.DataFrame)
   DICT: '#fb923c', // orange
   JSON_FILE: '#b45309', // amber
 
-  '2D': '#22d3ee', // cyan (Matrix, 2D np.ndarray)
+  '2D': '#22d3ee', // cyan (Matrix, 2D np.NdArray)
   DICOM_OBJ: '#2dd4bf', // teal
   DICOM_FILE: '#0f766e', // teal
 
@@ -18,7 +18,7 @@ export const DATATYPE_COLOR = {
   SERIES_FILE_LIST: '#ec4899', // pink
   STUDY_FILE_LIST: '#8b5cf6', // violet
 
-  '3D': '#e879f9', // fuchsia (Volume, 3D np.ndarray)
+  '3D': '#e879f9', // fuchsia (Volume, 3D np.NdArray)
   NIFTI_OBJ: '#db2777', // pink
   NIFTI_FILE: '#be185d', // pink
   // LABELMAP: '#86efac', // green
